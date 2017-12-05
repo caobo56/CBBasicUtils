bd9b7408465cb00f07422d602f70ab897506a677
caobo <bo.cao@Superd3d.com>
2017-12-05 17:47:02 +0800

    util

---
+++ src/Util/MentoreUitls.m
@@ -0,0 +1,22 @@
+//
+//  MentoreUitls.m
+//  CorderAlmanac
+//
+//  Created by caobo56 on 2017/12/1.
+//  Copyright © 2017年 caobo56. All rights reserved.
+//
+
+#import "MentoreUitls.h"
+#import "DateUtils.h"
+@implementation MentoreUitls
+
+
++(NSInteger)randomByDay{
+//    NSDate * date = [NSDate dateWithTimeIntervalSinceNow:0];
+//    date = [DateUtils dateToDay:date];
+//    long th = [date timeIntervalSince1970];
+//    srand((unsigned)time(&th));
+    return  rand();
+}
+
+@end
