d5ba17d4351477d0cfb347b96b82efc97191f383
caobo <bo.cao@Superd3d.com>
2017-12-06 08:46:37 +0800

    header++

---
+++ src/Requset/CBNetworkHeader.h
@@ -0,0 +1,15 @@
+//
+//  CBNetworkHeader.h
+//  BasicUtilsTest
+//
+//  Created by caobo56 on 2017/12/6.
+//  Copyright © 2017年 caobo56. All rights reserved.
+//
+
+#ifndef CBNetworkHeader_h
+#define CBNetworkHeader_h
+
+#import "APIClient.h"
+#import "CBURLSession.h"
+
+#endif /* CBNetworkHeader_h */
