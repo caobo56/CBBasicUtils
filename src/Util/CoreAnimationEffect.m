d30b96e63301e8313d913a26ddd47391e67a8434
caobo <caobo56@sina.com>
2017-12-12 21:08:45 +0800

    param warning —

--- src/Util/CoreAnimationEffect.m
+++ src/Util/CoreAnimationEffect.m
@@ -39,7 +39,7 @@
      *  @see CAAnimationDelegate    (按下command键点击)
      */
     
-    animation.delegate = self;
+//    animation.delegate = self;
     
     /** duration
      *
@@ -434,62 +434,6 @@
           }];
      }];
 }
-
-/** CABasicAnimation
- *
- *  @see https://developer.apple.com/library/mac/#documentation/cocoa/conceptual/CoreAnimation_guide/Articles/KVCAdditions.html
- *
- *  @brief                      便利构造函数 animationWithKeyPath: KeyPath需要一个字符串类型的参数,实际上是一个
- *                              键-值编码协议的扩展,参数必须是CALayer的某一项属性,你的代码会对应的去改变该属性的效果
- *                              具体可以填写什么请参考上面的URL,切勿乱填!
- *                              例如这里填写的是 @"transform.rotation.z" 意思就是围绕z轴旋转,旋转的单位是弧度.
- *                              这个动画的效果是把view旋转到最小,再旋转回来.
- *                              你也可以填写@"opacity" 去修改透明度...以此类推.修改layer的属性,可以用这个类.
- *
- *  @param toValue              动画结束的值.CABasicAnimation自己只有三个属性(都很重要)(其他属性是继承来的),分别为:
- *                              fromValue(开始值), toValue(结束值), byValue(偏移值),
- !                              这三个属性最多只能同时设置两个;
- *                              他们之间的关系如下:
- *                              如果同时设置了fromValue和toValue,那么动画就会从fromValue过渡到toValue;
- *                              如果同时设置了fromValue和byValue,那么动画就会从fromValue过渡到fromValue + byValue;
- *                              如果同时设置了byValue  和toValue,那么动画就会从toValue - byValue过渡到toValue;
- *
- *                              如果只设置了fromValue,那么动画就会从fromValue过渡到当前的value;
- *                              如果只设置了toValue  ,那么动画就会从当前的value过渡到toValue;
- *                              如果只设置了byValue  ,那么动画就会从从当前的value过渡到当前value + byValue.
- *
- *                              可以这么理解,当你设置了三个中的一个或多个,系统就会根据以上规则使用插值算法计算出一个时间差并
- *                              同时开启一个Timer.Timer的间隔也就是这个时间差,通过这个Timer去不停地刷新keyPath的值.
- !                              而实际上,keyPath的值(layer的属性)在动画运行这一过程中,是没有任何变化的,它只是调用了GPU去
- *                              完成这些显示效果而已.
- *                              在这个动画里,是设置了要旋转到的弧度,根据以上规则,动画将会从它当前的弧度专旋转到我设置的弧度.
- *
- *  @param duration             动画持续时间
- *
- *  @param timingFunction       动画起点和终点之间的插值计算,也就是说它决定了动画运行的节奏,是快还是慢,还是先快后慢...
- */
-
-/** CAAnimationGroup
- *
- *  @brief                      顾名思义,这是一个动画组,它允许多个动画组合在一起并行显示.比如这里设置了两个动画,
- *                              把他们加在动画组里,一起显示.例如你有几个动画,在动画执行的过程中需要同时修改动画的某些属性,
- *                              这时候就可以使用CAAnimationGroup.
- *
- *  @param duration             动画持续时间,值得一提的是,如果添加到group里的子动画不设置此属性,group里的duration会统一
- *                              设置动画(包括子动画)的duration属性;但是如果子动画设置了duration属性,那么group的duration属性
- *                              的值不应该小于每个子动画中duration属性的值,否则会造成子动画显示不全就停止了动画.
- *
- *  @param autoreverses         动画完成后自动重新开始,默认为NO.
- *
- *  @param repeatCount          动画重复次数,默认为0.
- *
- *  @param animations           动画组(数组类型),把需要同时运行的动画加到这个数组里.
- *
- *  @note  addAnimation:forKey  这个方法的forKey参数是一个字符串,这个字符串可以随意设置.
- *
- *  @note                       如果你需要在动画group执行结束后保存动画效果的话,设置 fillMode 属性,并且把
- *                              removedOnCompletion 设置为NO;
- */
 
 + (void)animationRotateAndScaleDownUp:(UIView *)view
 {
