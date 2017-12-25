deca3c20b1dec87f13e2297ceaa596a8afed9423
caobo <caobo56@sina.com>
2017-12-25 19:53:56 +0800

    1.0.02

--- src/Catergory/UIView+screenshots.m
+++ src/Catergory/UIView+screenshots.m
@@ -14,11 +14,12 @@
  */
 - (UIImage *)p_getFullScreenshots
 {
-    UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, 0.0);
+    CGSize s = self.bounds.size;
+    // 下面方法，第一个参数表示区域大小。第二个参数表示是否是非透明的。如果需要显示半透明效果，需要传NO，否则传YES。第三个参数就是屏幕密度了
+    UIGraphicsBeginImageContextWithOptions(s, NO, [UIScreen mainScreen].scale);
     [self.layer renderInContext:UIGraphicsGetCurrentContext()];
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIImage*image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
-    image = [UIImage imageWithCGImage:CGImageCreateWithImageInRect(image.CGImage,self.frame)];
     return image;
 }
 
