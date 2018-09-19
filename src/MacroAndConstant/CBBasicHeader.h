381c1182f442afb630a765f8fd2b29f1d2904a48
caobo <caobo56@sina.com>
2018-09-19 08:20:11 +0800

    s.version      = "1.0.17”

--- src/MacroAndConstant/CBBasicHeader.h
+++ src/MacroAndConstant/CBBasicHeader.h
@@ -9,6 +9,10 @@
 #ifndef CBBasicHeader_h
 #define CBBasicHeader_h
 
+#define IS_IPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+#define IS_RETINA ([[UIScreen mainScreen] scale] >= 2.0)
+
 #define SCREENSIZE_IS_35  (UI_IS_IPHONE && [[UIScreen mainScreen] bounds].size.height < 568.0)
 #define SCREENSIZE_IS_40  (UI_IS_IPHONE && [[UIScreen mainScreen] bounds].size.height == 568.0)
 #define SCREENSIZE_IS_47  (UI_IS_IPHONE && [[UIScreen mainScreen] bounds].size.height == 667.0)
@@ -20,18 +24,11 @@
 #define SCREEN_MAX_LENGTH (MAX(SCREEN_WIDTH, SCREEN_HEIGHT))
 #define SCREEN_MIN_LENGTH (MIN(SCREEN_WIDTH, SCREEN_HEIGHT))
 
-#define IS_IPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
-#define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
-#define IS_RETINA ([[UIScreen mainScreen] scale] >= 2.0)
-
 //状态栏、导航栏、标签栏高度
 #define Height_StatusBar [[UIApplication sharedApplication] statusBarFrame].size.height
-
 #define Height_NavBar self.navigationController.navigationBar.frame.size.height
-
 #define Height_TopBar (Height_StatusBar+Height_NavBar)
-
-#define TABBAR_HEIGHT (self.tabBarController.tabBar.frame.size.height)
+#define Height_TapBar (self.tabBarController.tabBar.frame.size.height)
 
 #define RGB16(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
